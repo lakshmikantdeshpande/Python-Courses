33624816b15345826333a573f04c9bbe3af61293
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-06-25 12:27:16 +0530

    Generate Random Data

---
+++ Python-Tutorial/numpy_basics/generate_random_data.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# parameters: distribution mean, standard deviation, number of samples to generate
+height = np.round(np.random.normal(1.75, 0.20, 5000), 2)
+weight = np.round(np.random.normal(60.32, 15, 5000), 2)
+np_city = np.column_stack((height, weight))
+
+print(np_city)
