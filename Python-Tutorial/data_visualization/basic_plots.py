f6456b25c86376304eaff4c9be752c5f43987ec2
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-06-26 14:58:59 +0530

    MatPlotLib basic line and scatter plots

---
+++ Python-Tutorial/data_visualization/basic_plots.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+year = [1950, 1970, 1990, 2010]
+
+pop = [2.519, 3.692, 5.263, 6.972]
+
+plt.plot(year, pop)
+
+plt.show()
+
+plt.scatter(year, pop)
+
+plt.show()
