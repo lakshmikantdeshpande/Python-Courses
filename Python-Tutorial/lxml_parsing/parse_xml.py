b8db24120b96f9d9800cf9b1546d55e2ee43286c
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-08-10 16:44:58 +0530

    fast xml parsing

--- Python-Tutorial/lxml_parsing/parse_xml.py
+++ Python-Tutorial/lxml_parsing/parse_xml.py
@@ -1,7 +1,39 @@
-# http://samples.openweathermap.org/data/2.5/weather?q=London&mode=xml&appid=b1b15e88fa797225412429c1c50c122a1
 from lxml import etree
 
-url = "http://samples.openweathermap.org/data/2.5/weather?q=London&mode=xml&appid=b1b15e88fa797225412429c1c50c122a1"
-doc = etree.parse(url)
 
-print(etree.tostring(doc))
+def all_children(context):
+    path = []
+    for event, elem in context:
+        if event == "start":
+            path.append(elem.tag)
+        else:
+            print_keyval(path, elem)
+            elem.clear()
+
+            while elem.getprevious() is not None:
+                if elem.getparent() is not None:
+                    del elem.getparent()[0]
+                else:
+                    break
+    del context
+
+
+def print_keyval(path, elem):
+    if elem is not None and elem.text is not None and elem.text.strip() != "":
+        print(".".join(path) + "==" + elem.text.strip())
+    try:
+        path.pop()
+    except IndexError:
+        pass
+
+
+if __name__ == "__main__":
+    source = "./en.xml"
+
+    # iterable context
+    context = etree.iterparse(source, events=("start", "end",))
+    # converted to iterator
+    context = iter(context)
+    event, root = next(context)  # to skip the parent tag
+
+    all_children(context)
