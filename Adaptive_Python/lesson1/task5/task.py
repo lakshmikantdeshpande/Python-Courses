b4b00c365cec1a00f16c4daa2eb75844c025d658
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-09-25 17:11:15 +0530

    Sort tuples based on their sum

--- Adaptive_Python/lesson1/task5/task.py
+++ Adaptive_Python/lesson1/task5/task.py
@@ -1,1 +1,9 @@
 # put your python code here
+if __name__ == "__main__":
+    n = int(input())
+    numbers = list()
+    for i in range(n):
+        numbers.append(tuple(map(int, input().split())))
+    numbers.sort(key=lambda x: round((x[0] ** (2) + x[1] ** (2)) ** (1.2)))
+    for number in numbers:
+        print("{0} {1}".format(number[0], number[1]))
