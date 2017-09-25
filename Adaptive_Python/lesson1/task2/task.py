eb0b8d4f246a27837cd95ca0d957e30321c08d1e
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-09-25 16:29:50 +0530

    Uppercase solved

--- Adaptive_Python/lesson1/task2/task.py
+++ Adaptive_Python/lesson1/task2/task.py
@@ -1,1 +1,8 @@
 # put your python code here
+def convert(temp):
+    print(temp.upper() if temp.islower() else (temp.lower() if temp.isupper() else temp))
+
+
+if __name__ == "__main__":
+    temp = input()
+    convert(temp)
