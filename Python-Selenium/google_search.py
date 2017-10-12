e0874db8a97722ad1e862b8edb9c8bde75fe4f63
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-10-12 21:05:22 +0530

    First Selenium script Python

---
+++ Python-Selenium/google_search.py
@@ -0,0 +1,11 @@
+import time 
+from selenium import webdriver
+
+driver = webdriver.Chrome("/usr/local/chromedriver-Linux64")
+driver.get("http://www.google.com/xhtml")
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
