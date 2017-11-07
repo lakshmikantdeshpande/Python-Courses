c4ac1835a54d9462b2087b29a81a77e34fdd2547
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-11-07 12:25:43 +0530

    Sample custom module import

---
+++ Courses/modules/callee.py
@@ -0,0 +1,7 @@
+def square(number):
+	return number ** 2
+
+
+def average(num1, num2):
+	return (num1 + num2) / 2;
+
---
+++ Courses/modules/caller.py
@@ -0,0 +1,9 @@
+from callee import *
+
+number = int(input("Enter a number: "))
+print(square(number))
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print(average(num1, num2))
+
