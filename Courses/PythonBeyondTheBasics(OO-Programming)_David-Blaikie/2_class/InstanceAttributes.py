3ff076fe22828c9728d979b1ee437cace95ca9c0
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-11-08 12:11:42 +0530

    Python Instance Attributes

---
+++ Courses/PythonBeyondTheBasics(OO-Programming)_David-Blaikie/2_class/InstanceAttributes.py
@@ -0,0 +1,11 @@
+import random
+
+class InstanceAttributes(object):
+	def dothis(self):
+		self.rand = random.randint(11, 101)
+
+
+instance = InstanceAttributes()
+instance.dothis()
+print(instance.rand)
+
