95592129e90e1f63f19ba616c2743bbb61cb60d4
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-11-16 21:52:12 +0530

    Logging in Python
    Console and File

---
+++ Courses/PythonBeyondTheBasics(OO-Programming)_David-Blaikie/10_Logging/LoggingDemo.py
@@ -0,0 +1,9 @@
+import logging
+
+# Default level is warning
+logging.basicConfig(level=logging.INFO)
+
+logging.debug('DEBUG')
+logging.info('INFO')
+logging.warning('WARN')
+
---
+++ Courses/PythonBeyondTheBasics(OO-Programming)_David-Blaikie/10_Logging/LoggingFile.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='log.log', filemode='w')
+
+logging.debug('DEBUG')
+logging.info('INFORMATION')
+logging.warning('WARNING')
+
