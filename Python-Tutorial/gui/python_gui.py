d15364b42c182d3487532853bde37deb48865494
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-12-31 12:25:53 +0530

    Python Tkinter GUI

---
+++ Python-Tutorial/gui/python_gui.py
@@ -0,0 +1,41 @@
+from tkinter import ttk, Tk, StringVar, W, E, S, N
+
+
+def calculate():
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5) / 10000.0)
+    except ValueError:
+        print("Invalid value")
+        pass
+
+
+# Root object
+root = Tk()
+root.title("Feet to Meters Converter")
+
+mainframe = ttk.Frame(root, padding="3 3 20 20")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is ").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind('<Return>', calculate)
+
+root.mainloop()
